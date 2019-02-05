a4486ad2a8f67b65c85a19925a1cd6d3a1831700
s181173 <s181173@student.dhbw-mannheim.de>
2019-02-05 13:08:12 +0100

    Hallo

--- WpfApplication1/WpfApplication1/obj/Debug/MainWindow.g.i.cs
+++ WpfApplication1/WpfApplication1/obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,22 @@ namespace WpfApplication1 {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 10 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.CheckBox chkStudent;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 11 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.ComboBox cbxHochschule;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +84,15 @@ namespace WpfApplication1 {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.chkStudent = ((System.Windows.Controls.CheckBox)(target));
+            return;
+            case 2:
+            this.cbxHochschule = ((System.Windows.Controls.ComboBox)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
